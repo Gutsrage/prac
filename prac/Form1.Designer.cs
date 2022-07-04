17535062f37f00c39196b37f01cdc3af1cfeaf36
Madara Uchiha <izidyzman@gmail.com>
2022-07-04 18:35:06 +0300

    Добавьте файлы проекта.

---
+++ prac/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace prac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pictureBox1.Location = new System.Drawing.Point(202, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(598, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите количество городов:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 68);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(184, 347);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(67, 421);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Расчитать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Город";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(164, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ prac/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int k; //количество городов 
+
+        List<TextBox> t_x = new List<TextBox>(); // текст боксы координаты городов 
+        List<TextBox> t_y = new List<TextBox>();
+        List<Label> l_q = new List<Label>();
+
+        int[] x;// коордитаны
+        int[] y;
+
+        List<double[]> mass = new List<double[]>();//города и расстояния между ними
+
+        List<double[]> ready = new List<double[]>();//города которые подсоеденены 
+
+        List<int> ready_1 = new List<int>();
+        List<int> ready_2 = new List<int>();//общая длинна кабеля 
+
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            foreach (TextBox tb in t_x)
+                tableLayoutPanel1.Controls.Remove(tb); //удоляет текстобоксы если нужно добавить городов
+            foreach (TextBox tb in t_y)
+                tableLayoutPanel1.Controls.Remove(tb);
+            foreach (Label lb in l_q)
+                tableLayoutPanel1.Controls.Remove(lb);
+            l_q.Clear();
+            t_x.Clear();
+            t_y.Clear();
+
+            try //задает количество городов
+            {
+                k = Convert.ToInt32(textBox1.Text);
+                tableLayoutPanel1.RowCount = k;
+                for (int i = 0; i < k; i++)
+                {
+                    Label l = new Label();
+                    string str = ("Город " + (i+1).ToString());
+                    l.Text = (str);
+                    l_q.Add(l);
+
+                    tableLayoutPanel1.Controls.Add(l, 0, i);
+                    TextBox textBox_x = new TextBox();
+                    t_x.Add(textBox_x);
+                        tableLayoutPanel1.Controls.Add(textBox_x, 1, i);
+                    
+
+                    TextBox textBox_y = new TextBox();
+                    t_y.Add(textBox_y);
+                    tableLayoutPanel1.Controls.Add(textBox_y, 2, i);
+                }
+                
+            }
+            catch
+            {
+                MessageBox.Show("Введено неверное число или введены не все коодинаты");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double sum = 0;
+            ready_1.Clear();//очистка выполненной работы 
+            ready_2.Clear();
+            ready.Clear();
+            x = new int[k];
+            y = new int[k];
+            int i = 0;
+            try
+            {
+                foreach (TextBox t in t_x) //вписываем в массивы координаты городов
+                {
+                    x[i] = Convert.ToInt32(t.Text);
+                    i++;
+                }
+                i = 0;
+                foreach (TextBox t in t_y)
+                {
+                    y[i] = Convert.ToInt32(t.Text);
+                    i++;
+                }
+                for (int a = 0; a < k-1; a++) //длинны соеденений 
+                {
+                    for (int b = a+1; b < k; b++)
+                    {
+                        double[] temp = new double[3];
+                        temp[0] = a;
+                        temp[1] = b;
+                        temp[2] = Math.Sqrt(Math.Pow(y[b]-y[a],2) + Math.Pow(x[b] - x[a], 2));
+                        mass.Add(temp);
+                    }
+                }
+                sort(mass);
+                for (i=0; i<k;i++)
+                {
+                    if (check_1(ready_1, ready_2, Convert.ToInt32(mass[i][0]), Convert.ToInt32(mass[i][1])))
+                    {
+                        ready_1.Add(Convert.ToInt32(mass[i][0]));
+                        ready_2.Add(Convert.ToInt32(mass[i][1]));
+                        check(ready_1, ready_2, Convert.ToInt32(mass[i][0]), Convert.ToInt32(mass[i][1]));
+                        ready.Add(mass[i]);
+                        sum += mass[i][2];
+                    }
+                }
+                pictureBox1.Invalidate();
+                MessageBox.Show("Длинна наименьшего пути: "+sum.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Неправильный ввод" + ex.ToString());
+                x = new int[k];
+                y = new int[k];
+            }
+
+            
+        }
+
+        private void sort(List<double[]> mass) //сортировка методом пузырька
+        {
+            double[] temp = new double[3];
+            for(int i = 0; i < mass.Count; i++)
+            {
+                for(int j = i+ 1; j< mass.Count;j++ )
+                {
+                    if (mass[i][2] > mass[j][2])
+                    {
+                        temp = mass[i];
+                        mass[i] = mass[j];
+                        mass[j] = temp;
+                    }
+                }
+            }
+        }
+
+        void check(List<int> ready_1, List<int> ready_2, int a, int b) //проверка на связи
+        {
+            List<int> temp = new List<int>();
+            for (int i = 0; i < ready_1.Count; i++)
+            {
+                if (a == ready_1[i])
+                    temp.Add(ready_2[i]);
+            }
+            foreach (int i in temp)
+            {
+                if (check_1(ready_1, ready_2, b, i))
+                {
+                    ready_1.Add(Math.Min(b, i));
+                    ready_2.Add(Math.Max(b, i));
+                }
+            }
+            temp.Clear();
+            for (int i = 0; i < ready_1.Count; i++)
+            {
+                if (b == ready_2[i])
+                    temp.Add(ready_1[i]);
+            }
+            foreach (int i in temp)
+            {
+                if (check_1(ready_1, ready_2, a, i))
+                {
+                    ready_1.Add(Math.Min(a, i));
+                    ready_2.Add(Math.Max(a, i));
+                }
+            }
+            temp.Clear();
+            for (int i = 0; i < ready_1.Count; i++)
+            {
+                if (a == ready_2[i])
+                    temp.Add(ready_1[i]);
+            }
+            foreach (int i in temp)
+            {
+                if (check_1(ready_1, ready_2, b, i))
+                {
+                    ready_1.Add(Math.Min(b, i));
+                    ready_2.Add(Math.Max(b, i));
+                }
+            }
+            temp.Clear();
+            for (int i = 0; i < ready_1.Count; i++)
+            {
+                if (b == ready_1[i])
+                    temp.Add(ready_2[i]);
+            }
+            foreach (int i in temp)
+            {
+                if (check_1(ready_1, ready_2, a, i))
+                {
+                    ready_1.Add(Math.Min(a, i));
+                    ready_2.Add(Math.Max(a, i));
+                }
+            }
+            temp.Clear();
+        }
+
+        private bool check_1(List<int> ready_1, List<int> ready_2, int a, int b)//проверка на соеденение в списке
+        {
+            for (int i = 0; i < ready_1.Count; i++)
+            {
+                if (ready_1[i] == a && ready_2[i] == b)
+                    return false;
+            }
+            return true;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)//рисование точек и линий
+        {
+            try
+            {
+                float max = Math.Max(Math.Max(Math.Abs(x.Max()), Math.Abs(x.Min())), Math.Max(Math.Abs(y.Max()), Math.Abs(y.Min())));
+                float k_1 = ((Math.Min(pictureBox1.Width, pictureBox1.Height) / 2) / max) * 0.98f;
+                Graphics g = e.Graphics;
+                Brush br = new SolidBrush(Color.Black);
+                Pen p = new Pen(Color.Coral);
+                foreach (double[] d in ready)
+                {
+                    g.FillEllipse(br, x[Convert.ToInt32(d[0])]*k_1 + pictureBox1.Width / 2 - 3, pictureBox1.Height / 2 - y[Convert.ToInt32(d[0])]*k_1 - 3, 6, 6);
+                    g.FillEllipse(br, x[Convert.ToInt32(d[1])]*k_1 + pictureBox1.Width / 2 - 3, pictureBox1.Height / 2 - y[Convert.ToInt32(d[1])]*k_1 - 3, 6, 6);
+                    g.DrawLine(p, x[Convert.ToInt32(d[0])]*k_1 + pictureBox1.Width / 2, pictureBox1.Height / 2 - y[Convert.ToInt32(d[0])]*k_1,
+                        x[Convert.ToInt32(d[1])] * k_1 + pictureBox1.Width / 2, pictureBox1.Height / 2 - y[Convert.ToInt32(d[1])] * k_1);
+                }
+            }
+            catch
+            {
+                
+            }
+        }
+    }
+}
